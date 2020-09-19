5cbefc4ac744dfe5c97f284a08877203c05517c9
José Neto <josenetopr5@gmail.com>
2020-09-19 10:09:41 -0300

    Bottom tab navigator

--- src/pages/Following/styles.ts
+++ src/pages/Following/styles.ts
@@ -1,3 +1,20 @@
-import styled from 'styled-components';
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
 
-export const Container = styled.div``;
+const statusBarHeight = Platform.OS === 'android' ? Constants.statusBarHeight : 0;
+
+export const Wrapper = styled.SafeAreaView`
+  background: ${colors.primary};
+  flex: 1;
+  padding-top: ${statusBarHeight + 'px'};
+`;
+
+export const Container = styled.View`
+  padding-left: 14px;
+`;
+
+export const Main = styled.View`
+  
+`;
